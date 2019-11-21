55500e708f5be605ca68baa190cf7ddd600d821f
Ruiz Pontes <ruizpontesa@student.douglascollege.ca>
2019-11-21 11:42:17 -0800

    Show info of client

--- app/src/main/java/com/warehousemanager/ui/client/account/AccountFragment.java
+++ app/src/main/java/com/warehousemanager/ui/client/account/AccountFragment.java
@@ -6,22 +6,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.warehousemanager.R;
+import com.warehousemanager.data.db.WarehouseDatabase;
+import com.warehousemanager.data.db.entities.User;
 
 public class AccountFragment extends Fragment {
 
-
-    public AccountFragment() {
-        // Required empty public constructor
-    }
-
+    private EditText name;
+    private EditText email;
+    private WarehouseDatabase warehouseDatabase;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_client_account, container, false);
+        View view = inflater.inflate(R.layout.fragment_client_account, container, false);
+        warehouseDatabase  = WarehouseDatabase.getAppDatabase(getActivity().getApplicationContext());
+
+        User user = warehouseDatabase.userDao().getUser();
+        name = view.findViewById(R.id.editTextClientName);
+        name.setText(user.getName());
+        email = view.findViewById(R.id.editTextClientUsername);
+        email.setText(user.getUsername());
+
+        return view;
     }
 
 }
