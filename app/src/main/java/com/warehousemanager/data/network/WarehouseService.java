a6e86b07bf2bf5724b8f8751f62b2d92a10258ff
Oliveira Reboucas <oliveirareboucasc@student.douglascollege.ca>
2019-11-27 15:24:33 -0800

    Update IP amazonAWS

--- app/src/main/java/com/warehousemanager/data/network/WarehouseService.java
+++ app/src/main/java/com/warehousemanager/data/network/WarehouseService.java
@@ -12,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class WarehouseService {
 
     //final static String IPADDRESS = "http://140.161.215.16:8000";
-    final static String IPADDRESS = "http://140.161.215.91:8000"; // This IP address doesn't change
+    final static String IPADDRESS = "http://ec2-3-87-64-207.compute-1.amazonaws.com:8000"; // This IP address doesn't change
 
     public static Retrofit retrofit;
 
